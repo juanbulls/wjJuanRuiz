f725b719317a841b4725b6be6da936ca9f2c0fdc
juanbulls <juanbulls@yahoo.com>
2019-02-04 15:36:29 -0500

    buscando error en fila 16843

--- apl.py
+++ apl.py
@@ -5,12 +5,22 @@ paralineas.close()
 adatos = open(r'blogtext.csv', 'r', encoding='utf-8')
 datos = csv.reader(adatos)
 
+tema = []
 c=0
 l=6
 print('Muestra de las '+str(l-1)+' primeras lineas de las ' + str(lineas) + ' del blog:')
-for f in datos:
-    if c==l:
-        break
-    print(f[5])
-    c+=1
+try:
+    for f in datos:
+        #if c==l:
+        #    break
+        unico = True
+        for t in tema:
+            if t == f[3]:
+                unico = False
+        if unico == True:
+            tema.append(f[3])
+            print(str(c) + ' incluido: ' + f[3])
+        c+=1
+except:
+    print("Error en la fila " + str(c))
 print('termine')
