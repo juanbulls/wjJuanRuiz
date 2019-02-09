139c5f4878c4eaac6310883b383929361717d7a9
juanbulls <juanbulls@yahoo.com>
2019-02-08 21:07:47 -0500

    avances oficina 2

--- por tema y edad.py
+++ por tema y edad.py
@@ -1,7 +1,8 @@
 import csv
 import sys
+url = r'../blogtext.csv'
 csv.field_size_limit(sys.maxsize)
-adatos = open(r'blogtext.csv', 'r', encoding='utf-8')
+adatos = open(url, 'r', encoding='utf-8')
 datos=csv.reader(x.replace('\0','') for x in adatos)
 
 tema = []
@@ -22,7 +23,7 @@ for i,f in enumerate(datos):
         t+=1
 print ("indexando edades...")
 adatos.close()
-adatos = open(r'blogtext.csv', 'r', encoding='utf-8')
+adatos = open(url, 'r', encoding='utf-8')
 datos=csv.reader(x.replace('\0','') for x in adatos)
 for p,g in enumerate(datos):
     unico = True
@@ -35,13 +36,32 @@ for p,g in enumerate(datos):
         edad[e].append(g[2])
         edad[e].append(1)
         e+=1
+adatos.close()
 print ()
 print('temas de mayor a menor:')
 temas = sorted(tema,key=lambda x: x[1], reverse=True)
-for i in range(t):
+for i in range(t-1):
     print(temas[i][0]+' '+str(temas[i][1]))
 print()
 print('edades en orden:')
 edades = sorted(edad,key=lambda x: x[0], reverse=False)
-for i in range(e):
+for i in range(e-1):
     print(edades[i][0]+' '+str(edades[i][1]))
+print()
+print('escribiendo temas a csv')
+temasf = open('temas.csv', 'w')
+temasf.write('Tema, Menciones')
+for i in range(t-1):
+    temasf.write('\n')
+    temasf.write(temas[i][0]+','+str(temas[i][1]))
+temasf.close()
+print('escrito en temas.csv')
+print()
+print('escribiendo edades a csv')
+edadesf = open('edades.csv','w')
+edadesf.write('Edad,Menciones')
+for i in range(e-1):
+    edadesf.write('\n')
+    edadesf.write(str(edades[i][0])+','+str(edades[i][1]))
+edadesf.close()
+print('escrito en edades.csv')
