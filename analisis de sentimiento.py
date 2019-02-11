b7068b92197c21a573267463bc478be81c013d02
juanbulls <juanbulls@yahoo.com>
2019-02-10 20:31:11 -0500

    sentimiento terminado

--- analisis de sentimiento.py
+++ analisis de sentimiento.py
@@ -12,6 +12,10 @@ def barra(titulo):
 print('\nAplicacion para Whale & Jaguar por Juan Ruiz.')
 print('Sentimiento: positivo y negativo')
 print('------------------------------------------------')
+print('El siguiente codigo tarda aproximadamente 45 minutos,')
+print('utilice el csv sentimiento, si no de sea esperar.')
+salida = input('si desea salir escriba salir, enter de lo contrario:\n')
+if salida == 'salir' or salida == 'Salir': sys.exit()
 print('calculando largo...', end='')
 largo = sum(1 for line in open(url, encoding='utf-8'))
 print(str(largo)+' lineas\n')
@@ -61,17 +65,20 @@ for a,b in enumerate(datos):
         print('-', end='', flush=True)
     pos=0
     neg=0
+    np = 0
     for np in range(len(positivas)):
-        if positivas[np] in b[6]:
+        if b[6].find(positivas[np]) != -1:
             pos+=1
+    nn = 0
     for nn in range(len(negativas)):
-        if negativas[nn] in b[6]:
+        if b[6].find(negativas[nn]) != -1:
             neg+=1
     if pos!=0 or neg!=0:
+        k=0
         for k in range(len(tema)):
             if tema[k][0]==b[3]:
-                tema[k][2] = pos
-                tema[k][3] = neg
+                tema[k][2] += pos
+                tema[k][3] += neg
                 break
 print('-]')
 print ()
@@ -84,9 +91,9 @@ print('escribiendo sentimiento a csv')
 sentf = open('sentimiento.csv','w')
 sentf.write('Edad,Menciones')
 for i in range(t-1):
-    edadesf.write('\n')
-    edadesf.write(str(edades[i][0])+','+str(edades[i][1])+','+str(edades[i][2])+','+str(edades[i][3]))
-edadesf.close()
-print('escrito en edades.csv')
+    sentf.write('\n')
+    sentf.write(str(tema[i][0])+','+str(tema[i][1])+','+str(tema[i][2])+','+str(tema[i][3]))
+sentf.close()
+print('escrito en sentimiento.csv')
 print()
 print('Programa finalizado')
